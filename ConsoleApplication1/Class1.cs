172b0a0a17d31131ac307203e9e242cbe5651760
KonstantinGusev <konstantinvgusev@gmail.com>
2016-06-04 13:18:46 +0300

    Тотально передалано все

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// Класс для возврата нового и старого значения
+    /// Содержит события для обработчика событий
+    /// </summary>
+    public class ChangeNameAgeEventArg : EventArgs
+    {
+        public string OldName { get; set; }
+        public string NewName { get; set; }
+
+        public int OldAge { get; set; }
+        public int NewAge { get; set; }
+
+        public ChangeNameAgeEventArg(string oldName, string newName)
+        {
+            OldName = oldName;
+            NewName = newName;
+        }
+
+        public ChangeNameAgeEventArg(int oldAge, int newAge)
+        {
+            OldAge = oldAge;
+            NewAge = newAge;
+        }
+    }
+
+    /// <summary>
+    /// непосредственно сам класс который добавляет
+    /// изменяет или удаляет человеков
+    /// </summary>
+    class User
+    {
+
+        public string name;
+        public int age;
+
+        //Обработчики событий
+        public event EventHandler<ChangeNameAgeEventArg> OnChangeName;
+        public event EventHandler<ChangeNameAgeEventArg> OnChangeAge;
+
+        public User(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name {
+            get { return name; }
+
+            set {
+                if (OnChangeName != null)
+                {
+                    OnChangeName(this, new ChangeNameAgeEventArg(Name, value));
+                    name = value;
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// В свойство Age добавляем проверку на максимальный возраст
+        /// </summary>
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value > 150)
+                {
+                    throw new Exception(string.Format("Возраст не корректен", value));
+                }
+                else if (OnChangeAge != null)
+                {
+                    OnChangeAge(this, new ChangeNameAgeEventArg(Age, value));
+                    this.age = value;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user = new User("Костя", 280);
+            user.OnChangeName += OldNewName;
+            user.OnChangeAge += OldNewAge;
+            user.Name = "Ёжик";
+            user.Age = 40;
+            
+
+        }
+
+        public static void OldNewName(object s, ChangeNameAgeEventArg e)
+        {
+            Console.WriteLine("Старое значение = {0}, новое значение = {1}", e.OldName, e.NewName);
+        }
+
+        public static void OldNewAge(object s, ChangeNameAgeEventArg e)
+        {
+            Console.WriteLine("Старое значение = {0}, новое значение = {1}", e.OldAge, e.NewAge);
+        }
+    }
+}
